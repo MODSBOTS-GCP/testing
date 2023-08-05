a95f547bfcb6da39125177076a2cfd022e7204bd
MODSBOTS-GCP <141257681+MODSBOTS-GCP@users.noreply.github.com>
2023-08-05 23:02:10 +0630

    Update modsbots.sh

--- modsbots.sh
+++ modsbots.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 relay login -k a3870615-f02e-46c3-b0f8-ab240a1b90af -s mGRLLeOuyvu9
 echo "MODSBOTS IS THE KING"
- 
+nohup relay connect --region eu --name mygcps 
 #chmod +x /usr/local/bin/x-ui
-/usr/local/bin/x-ui run bash
-relay connect --region eu --name mygcps
+#/usr/local/bin/x-ui run bash
+
